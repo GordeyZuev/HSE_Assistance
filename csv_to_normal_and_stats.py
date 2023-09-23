6a1c6c9bcde7d8574d4da750b220874110105421
Gordey Zuev <58434126+GordeyZuev@users.noreply.github.com>
2023-09-24 00:38:40 +0300

    Update csv_to_normal_and_stats.py
    - Оптимизирован подсчет доли успешных попыток.
    - Добавлен подсчет студентов, выполнивших все задания
    - Исправлены Ошибки.
    - Добавлены Комментарии.

--- csv_to_normal_and_stats.py
+++ csv_to_normal_and_stats.py
@@ -8,7 +8,7 @@ with open("standings-52138 (1).csv", "r", encoding='utf-8') as f:
 
 
 # Функция для конвертации количества попыток в нормальный формат
-def result_converter(result):
+def result_converter(result: str) -> int:
     if result:              # Проверка на пустоту
         if result == '+':   # Случай, когда решена одна задача
             return 1
@@ -19,9 +19,10 @@ def result_converter(result):
 
 
 # Создаем переменные
-normal_list = []                    # Список с результатами по всем задачам
-result = {'Name': 'Solved'}         # Словарь для пар Имя - количество решенных
+normal_list = []                # Список с результатами по всем задачам
+result = {}                     # 'Name': 'Solved' - Словарь для пар Имя - количество решенных
 
+# Цикл, который обрабатывает строчку, убирая лишняя и форматируя данные
 for i in range(len(list)):
     line = list[i]
     normal_line = line.replace('"', '').split(',')
@@ -31,62 +32,65 @@ for i in range(len(list)):
 
     line_size = len(normal_line)
 
-    if i != 0:
+    if i != 0:      # Проверяем - не 0, так как normal_line[0] - имя студента
         for j in range(1, line_size - 1):
-            normal_line[j] = result_converter(normal_line[j])
-        normal_line[-1] = int(normal_line[-1])
+            normal_line[j] = result_converter(normal_line[j])       # Переделываем результат из строки в цифру.
+        normal_line[-1] = int(normal_line[-1])                      # Для результата правила иные - просто переделываем число в integer
 
-        result[normal_line[0]] = normal_line[-1]
+        result[normal_line[0]] = normal_line[-1]        # Добавляем в словарь result пару "Ученик" - "Балл"
 
-    normal_list.append(normal_line)
+    normal_list.append(normal_line)                     # Добавляем отформатированную строку в табличку.
 
-for name in group_list:
+for name in group_list:     # Проверяем, есть ли студент в списке группы
     if name in result:
-        pass
+        pass                # Студент есть Балл остается тем же
     else:
-        result[name] = 0
+        result[name] = 0    # Студента нет -> Балл = 0
 
 result = dict(sorted(result.items()))       # Сортируем по фамилии студентов.
-normal_list.sort()      # Тут сортировка работает хорошо, потому что идет по первому элементу!
+#normal_list.sort()      # Тут сортировка работает хорошо, потому что идет по первому элементу!
 
-
+# Выводим список студентов
 for name in result:
-    if name in group_list:
+    if name in group_list:      # Проверяем, есть ли студент в группе.
        print(result[name], '\t', name)
 
 
-
-
-
-# Статистика - Пока тестирую, есть некоторые проблемы.
-def contest_stats(normal_list):
+# Статистика - Тестовое.
+def contest_stats(normal_list: list):
     line_size = len(normal_list[0])
     solved_all_num = 0
 
     min_percentage = 1.0
     hardest_task = ''
 
-    tasks_list = {'Task': ['Solved', 'Attempts', 'Percentage']}
+    tasks_list = {}             # 'Task': ['Solved', 'Attempts', 'Percentage']
     for i in range(1, line_size - 1):
         tasks_list[normal_list[0][i]] = [0, 0, 0]
 
-    for i in range(1, len(normal_list) - 1):
-        for j in range(1, line_size - 1):
+    for i in range(1, len(normal_list)):
+        for j in range(1, line_size):
+            if j < line_size - 1:
+                if normal_list[i][j] > 0:
+                    tasks_list[normal_list[0][j]][0] += 1
 
-            if normal_list[i][j] > 0:
-                tasks_list[normal_list[0][j]][0] += 1
+                tasks_list[normal_list[0][j]][1] += abs(normal_list[i][j])
+            else:
+                if j - 1 == normal_list[i][j]:
+                    print(j, normal_list[i])
+                    solved_all_num += 1
 
-            tasks_list[normal_list[0][j]][1] += abs(normal_list[i][j])
 
-    for i in range(1, len(normal_list) - 1):
-        for j in range(1, line_size - 1):
-            percentage = round(tasks_list[normal_list[0][j]][0] / tasks_list[normal_list[0][j]][1], 3)
-            tasks_list[normal_list[0][j]][2] = percentage
+    # Добавляем долю успешных поссылок и определяем самую низкую
+    for task in tasks_list:
+        percentage = round(tasks_list[task][0] / tasks_list[task][1],3)
+        tasks_list[task][2] = percentage
 
-            if min_percentage > percentage:
-                min_percentage = percentage
-                hardest_task = (normal_list[0][j], min_percentage)
+        if min_percentage > percentage:             # Поиск самой низкой доли
+            min_percentage = percentage
+            hardest_task = (task, min_percentage)
 
+    # Оформление вывода
     print()
     for task in tasks_list:
         print(f'Задача {task.replace("(", " (")}')
@@ -94,5 +98,7 @@ def contest_stats(normal_list):
         print()
     print(f'Самая сложная - {hardest_task[0].replace("(", " (")}')
     print(f'Удачных попыток -  {hardest_task[1]}')
+    print(f'Решили все задачи - {solved_all_num}')
 
+# Вызов функции статистики.
 contest_stats(normal_list)
