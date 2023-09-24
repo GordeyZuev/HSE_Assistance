abdf33824c0fe042721047857355baab95212703
Gordey Zuev <58434126+GordeyZuev@users.noreply.github.com>
2023-09-24 13:00:29 +0300

    Update csv_to_normal_and_stats.py
    - Изменена структура
    - Добавлены комментарии к функции contest_stats
    - Добавлены переменные для хранения имен файлов

--- csv_to_normal_and_stats.py
+++ csv_to_normal_and_stats.py
@@ -1,9 +1,13 @@
+# Создаем переменные с именами файлов
+group_list_file = 'group_list.txt'
+contest_results_file = 'standings-51827 (5).csv'
+
 # Считываем список группы
-with open("group_list.txt", "r", encoding='utf-8') as f:
+with open(group_list_file, "r", encoding='utf-8') as f:
      group_list = f.read().split('\n')
 
 # Считываем csv с результатами контеста
-with open("standings-52138 (1).csv", "r", encoding='utf-8') as f:
+with open(contest_results_file, "r", encoding='utf-8') as f:
     list = f.read().split('\n')[:-1]
 
 
@@ -16,6 +20,58 @@ def result_converter(result: str) -> int:
             result = int(result)
             return result + 1 if result > 0 else result         # Если не решена - выводим как есть (с минусом), иначе + 1
     return 0                # Случай, когда решения нет
+
+# Статистика по контесту в целом - Тестовое.
+def contest_stats(normal_list: list):
+    participants_num = len(normal_list) - 1     # Количетсво Участников
+    line_size = len(normal_list[0])             # Размер строки - Имя студента + все задачи + результат
+    solved_all_num = 0                          # Количество решивших все
+    min_share = 1.0                             # Минимальная доля решивших все
+    hardest_task = ''                           # Ячейка под хранение имени самой сложной задачи
+
+    tasks_list = {}     # 'Task': ['Solved', 'Attempts', 'Share']
+
+    # Цикл, который заполняет названия задач.
+    for i in range(1, line_size - 1):
+        tasks_list[normal_list[0][i]] = [0, 0, 0]
+
+    # Цикл, который проходится по всей таблице и заполняет информацию о каждой задаче.
+    for i in range(1, len(normal_list)):
+        for j in range(1, line_size):
+            if j < line_size - 1:           # Проходимся по всем задачам - переносим информацию
+                if normal_list[i][j] > 0:
+                    tasks_list[normal_list[0][j]][0] += 1                   # Задача решена - добавляем еденичку.
+                tasks_list[normal_list[0][j]][1] += abs(normal_list[i][j])  # Добавляем количество попыток
+
+            else:                           # Заодно проходимся и по итоговым результатам - смотрим, сколько учеников сделали все задачи
+                if j - 1 == normal_list[i][j]:
+                    solved_all_num += 1
+
+
+    # Добавляем долю успешных поссылок и определяем самую низкую
+    for task in tasks_list:
+        share = round(tasks_list[task][0] / tasks_list[task][1],3)
+        tasks_list[task][2] = share
+
+        if min_share > share:             # Поиск самой низкой доли
+            min_share = share
+            hardest_task = (task, min_share)
+
+    # Оформление вывода
+    print('\n\n', '-' * 3 + 'Main Statistics' + '-' * 3, '\n')
+
+    print(f'Всего решали - {participants_num}')
+    print(f'Решили всё - {solved_all_num}')
+
+    print(f'\n- Самая сложная задача - {hardest_task[0].replace("(", " (")} человек.')
+    print(f'Удачных попыток -  {hardest_task[1]}')
+
+    print('\n\n', '-' * 3 + 'Task Statistics' + '-' * 3, '\n')
+
+    for task in tasks_list:
+        print(f'- Задача {task.replace("(", " (")}')
+        print(f'Решило: {tasks_list[task][0]}, Попыток: {tasks_list[task][1]}, Процент: {tasks_list[task][2]}')
+        print()
 
 
 # Создаем переменные
@@ -57,56 +113,5 @@ for name in result:
     if name in group_list:      # Проверяем, есть ли студент в группе.
        print(result[name], '\t', name)
 
-
-# Статистика по контесту в целом - Тестовое.
-def contest_stats(normal_list: list):
-    participants_num = len(normal_list) - 1
-    line_size = len(normal_list[0])
-    solved_all_num = 0
-
-    min_percentage = 1.0
-    hardest_task = ''
-
-    tasks_list = {}             # 'Task': ['Solved', 'Attempts', 'Percentage']
-    for i in range(1, line_size - 1):
-        tasks_list[normal_list[0][i]] = [0, 0, 0]
-
-    for i in range(1, len(normal_list)):
-        for j in range(1, line_size):
-            if j < line_size - 1:
-                if normal_list[i][j] > 0:
-                    tasks_list[normal_list[0][j]][0] += 1
-
-                tasks_list[normal_list[0][j]][1] += abs(normal_list[i][j])
-            else:
-                if j - 1 == normal_list[i][j]:
-                    solved_all_num += 1
-
-
-    # Добавляем долю успешных поссылок и определяем самую низкую
-    for task in tasks_list:
-        percentage = round(tasks_list[task][0] / tasks_list[task][1],3)
-        tasks_list[task][2] = percentage
-
-        if min_percentage > percentage:             # Поиск самой низкой доли
-            min_percentage = percentage
-            hardest_task = (task, min_percentage)
-
-    # Оформление вывода
-    print('\n\n', '-' * 3 + 'Main Statistics' + '-' * 3, '\n')
-
-    print(f'Всего решали - {participants_num}')
-    print(f'Решили всё - {solved_all_num}')
-
-    print(f'\n- Самая сложная задача - {hardest_task[0].replace("(", " (")} человек.')
-    print(f'Удачных попыток -  {hardest_task[1]}')
-
-    print('\n\n', '-' * 3 + 'Task Statistics' + '-' * 3, '\n')
-
-    for task in tasks_list:
-        print(f'- Задача {task.replace("(", " (")}')
-        print(f'Решило: {tasks_list[task][0]}, Попыток: {tasks_list[task][1]}, Процент: {tasks_list[task][2]}')
-        print()
-
 # Вызов функции статистики.
-contest_stats(normal_list)
+#contest_stats(normal_list)
