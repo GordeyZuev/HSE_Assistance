6b9f9cd335709c00d118bc6bc7d1ab1b9be3429a
Gordey Zuev <58434126+GordeyZuev@users.noreply.github.com>
2024-09-21 14:20:21 +0300

    New optimized version

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 import pandas as pd
 from result_converter import result_converter
 
-contest_file_name = 'standings-knad1.csv'
-
-# Достаем файлик, удаляем столбец penalty
-contest_results = pd.read_csv(contest_file_name).set_index('user_name')
-contest_results.drop(['Penalty','place'], axis = 1, inplace=True)
-students_list = pd.read_csv("students_list.csv").set_index('user_name')
-
-# Объединяем бд
-final_list = students_list.merge(contest_results, left_on='user_name', right_on='user_name', how='left')
-
-# Обрабатываем столбец с результатами.
-#final_list = final_list.fillna(0)
-for task in final_list.columns.tolist()[4:]:
-    final_list[task] = final_list[task].fillna(0).apply(result_converter)
-
-# Настройка формата отображения.
+# Настройка отображения.
 pd.set_option('display.max_columns', None)
 pd.options.display.float_format = '{:,.0f}'.format
-# Добавляем столбец с итогами (Суммой)
-final_list.loc['Total'] = final_list.sum(numeric_only=True)
 
-# Вывод
-faculty_choosing = 'КНАД241'
-export_result = (final_list[final_list['faculty'] == faculty_choosing])
+# Чтение файлов.
+contest_file_name = 'standings-knad1.csv'
+contest_results = pd.read_csv(contest_file_name).drop(columns=['Penalty', 'place'])
+students_list = pd.read_csv("students_list.csv")
+
+# Объединение данных.
+final_list = students_list.merge(contest_results, on=['user_name', 'login'], how='left').set_index('user_name')
+
+# Обработка столбцов с результатами.
+tasks = final_list.columns[final_list.columns.get_loc('faculty') + 1:]
+final_list[tasks] = final_list[tasks].fillna(0).map(result_converter)
+
+# Добавляем столбец с итогами.
+final_list.loc['Total'] = final_list.select_dtypes(include='number').sum()
+
+# Экспорт результата.
+faculty_choosing = '' # Варианты: 'КНАД241' / 'КНАД242' / 'ВСН' / 'СмолГУ'
+export_result = final_list[final_list['faculty'] == faculty_choosing]
 export_result.to_csv('result.csv')
