c074f5107599761cf4d071918f27d716f3255f76
Gordey Zuev <58434126+GordeyZuev@users.noreply.github.com>
2024-09-22 17:16:32 +0300

    Fix - Wrong results showing

--- main.py
+++ main.py
@@ -11,13 +11,23 @@ contest_results = pd.read_csv(contest_file_name).drop(columns=['Penalty', 'place
 students_list = pd.read_csv("students_list.csv")
 
 # Объединение данных.
-final_list = students_list.merge(contest_results, on=['user_name', 'login'], how='left').set_index('user_name')
+final_list = students_list.merge(contest_results, on=['user_name', 'login'], how='left').set_index('user_name').fillna(0)
+
+# Определение индексов столбцов с результатами.
+res_start_ind = final_list.columns.get_loc('faculty') + 1
+res_end_ind = final_list.columns.get_loc('Score')
 
 # Обработка столбцов с результатами.
-tasks = final_list.columns[final_list.columns.get_loc('faculty') + 1:]
-final_list[tasks] = final_list[tasks].fillna(0).map(result_converter)
+tasks = final_list.columns[res_start_ind:res_end_ind]
+final_list[tasks] = final_list[tasks].map(result_converter)
 
-# Выполняем сортировку..
+# Добавляем столбец с итогами.
+final_list.loc['Total'] = final_list.select_dtypes(include='number').sum()
+
+# Привеодим значения к типу int.
+final_list[final_list.columns[res_start_ind:]] = final_list[final_list.columns[res_start_ind:]].astype(int)
+
+# Сортировка значений.
 final_list.sort_values(by=['faculty', 'user_name'])
 
 # Экспорт результата.
